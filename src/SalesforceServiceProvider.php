048f593b06fc5e07e976440a17325d09e7554ac1
Hugo Hernández <hugo@doitcloud.consulting>
2019-04-29 16:26:59 -0500

    Upload Project

---
+++ src/SalesforceServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DoITCloudConsulting\Salesforce;
+
+use Illuminate\Support\ServiceProvider;
+
+class SalesforceServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/SalesforceConfig.php', 'SalesforceConfig'
+        );
+        $this->app->make('DoITCloudConsulting\Salesforce\Controllers\Salesforce');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
