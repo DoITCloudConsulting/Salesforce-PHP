90c4da5e12f14242043c65b9c30c9db026428ae9
Hugo Hernández <hugo@doitcloud.consulting>
2019-05-30 19:40:03 -0500

    change

--- src/config/SalesforceConfig.php
+++ src/config/SalesforceConfig.php
@@ -11,9 +11,9 @@ return [
     | 
     */
 
-	'Username' => 'mayax@doitcloud.consulting',
-	'Password' => 'trayecta85',
-	'Token' => 'IU2JyLDkiairgKI9G4Pap7a8',
+	'Username' => '--@---.consulting',
+	'Password' => '-----',
+	'Token' => '-----',
 
 	/*
     |--------------------------------------------------------------------------
